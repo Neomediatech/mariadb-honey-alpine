cfb7354e91abf93d8f08568c6d0c4dd4ff0d4d50
Dario B <feddar@users.noreply.github.com>
2020-09-03 15:33:28 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ rm -rf "${DB_DATA_PATH}"
 mkdir -p $DB_DATA_PATH ; chmod 777 $DB_DATA_PATH 
 chown -R mysql:mysql ${DB_DATA_PATH}
 
+mkdir -p /data/logs ; chmod 777 /data/logs ; touch /data/logs/mysql.log /data/logs/mysql-slow.log; chmod 666 /data/logs/mysql.log /data/logs/mysql-slow.log
+
 mysql_install_db --skip-name-resolve --user=mysql --datadir=${DB_DATA_PATH} > /dev/null
 
 DB_ROOT_PASS=`pwgen 24 1`
@@ -30,9 +32,6 @@ EOF
 /usr/bin/mysqld --user=mysql --bootstrap --verbose=1 < $tfile
 rm -f $tfile
 
-mkdir -p /data/logs ; chmod 777 /data/logs ; touch /data/logs/mysql.log /data/logs/mysql-slow.log; chmod 666 /data/logs/mysql.log /data/logs/mysql-slow.log
-
 tail -f /data/logs/mysql.log &
 
 exec /usr/bin/mysqld --user=mysql --console
-
